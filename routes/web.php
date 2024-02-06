a412164f7ffd62f862fea4d56cc434e93cdff08b
N1KART <arteni_nicu@mail.ru>
2024-02-06 11:11:54 +0200

    Relatii, migrare ,rute

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\PhoneController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +15,14 @@ use App\Http\Controllers\UserController;
 */
 Route::redirect('/', '/users');
 Route::get( '/users' , [UserController::class, 'index']) ->name('users.index');
-
 Route::get('/users/create' , [UserController::class , 'create']) ->name('users.create');
 Route::post('/users' , [UserController::class , 'store'])->name('users.store');
+Route::controller(PhoneController::class)->group(function(){
+    Route::get('phones', 'index');
+    Route::get('phones/create','create');
+    Route::post('phones','store');
+    Route::get('phones{phones}','show');
+    Route::get('phones/{phone}/edit','edit');
+    Route::pus('phones/{phone}/update','update');
+    Route::delete('phones/{phone}', 'destroy');
+});
